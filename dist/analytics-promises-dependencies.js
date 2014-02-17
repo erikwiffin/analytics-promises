4a7bf4e13a677f64bdf16e314caae2d18197b369
Erik Wiffin <erik.wiffin@gmail.com>
2014-02-17 22:43:55 +0000

    Improve playbook

--- dist/analytics-promises-dependencies.js
+++ dist/analytics-promises-dependencies.js
@@ -2526,7 +2526,7 @@ define("rsvp",
 global.RSVP = require('rsvp');
 }(window));
 /*!
- * analytics-promises 0.1.0
+ * analytics-promises 0.2.0
  * https://github.com/erikwiffin/analytics-promises
  * 
  * @copyright 201414, Erik Wiffin <erik.wiffin@gmail.com>
